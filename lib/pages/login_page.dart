cafcb38959f3a38554fed9501852f015f4bd7583
Dev Karan Singh <dev1604karan@gmail.com>
2024-10-16 00:15:55 +0530

    started login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: Column(
+          children: [
+            //logo
+
+            //welcome back message
+
+            //email textfield
+
+            //login button
+
+            //register now
+          ],
+        ));
+  }
+}
