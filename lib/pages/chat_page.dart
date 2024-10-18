0b41ff75e4f51f41772a84cd43b83762af310c95
Dev Karan Singh <dev1604karan@gmail.com>
2024-10-18 23:58:42 +0530

    creating chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:panchayat/services/auth/auth_service.dart';
+import 'package:panchayat/services/chat/chat_service.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
+  final String receiverID;
 
-  const ChatPage({
+  ChatPage({
     super.key,
     required this.receiverEmail,
+    required this.receiverID,
   });
+
+  //text controller
+  final TextEditingController _messageController = TextEditingController();
+
+  //chat and auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
+
+  //send message
+  void sendMessage() async {
+    //if there is something in the textfield
+    if (_messageController.text.isNotEmpty) {
+      //send message
+      await _chatService.sendMessage(receiverEmail, _messageController.text);
+
+      //clear textfield
+      _messageController.clear();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Center(
-          child: Text(receiverEmail),
-        ),
+        title: Text(receiverEmail),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          //display messages(majority)
+          Expanded(child: _buildMessageList(),),
+
+          //user input
+        ],
       ),
     );
+  }
+
+  //build message list
+  Widget _buildMessageList(){
+    String enderID = _authService.getCurrentUser()!uid;
+    return StreamBuilder(
+      
+      builder: ,)
   }
 }
