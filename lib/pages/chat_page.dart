0f4f74acc57d81b72ebb4e607c0090898987561f
Dev Karan Singh <dev1604karan@gmail.com>
2024-10-19 20:04:16 +0530

    list

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:panchayat/components/my_textfield.dart';
 import 'package:panchayat/services/auth/auth_service.dart';
 import 'package:panchayat/services/chat/chat_service.dart';
 
@@ -25,6 +27,7 @@ class ChatPage extends StatelessWidget {
     if (_messageController.text.isNotEmpty) {
       //send message
       await _chatService.sendMessage(receiverEmail, _messageController.text);
+      debugPrint('Message sent: ${_messageController.text}');
 
       //clear textfield
       _messageController.clear();
@@ -41,19 +44,73 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           //display messages(majority)
-          Expanded(child: _buildMessageList(),),
+          Expanded(
+            child: _buildMessageList(),
+          ),
 
           //user input
+          _buildUserInput(),
         ],
       ),
     );
   }
 
   //build message list
-  Widget _buildMessageList(){
-    String enderID = _authService.getCurrentUser()!uid;
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
     return StreamBuilder(
-      
-      builder: ,)
+      stream: _chatService.getMessages(receiverID, senderID),
+      builder: (context, snapshot) {
+        //errors
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        //return list view
+        return ListView(
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  //build message item
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        //textfield should take up most of the space
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 15.0),
+            child: MyTextField(
+              controller: _messageController,
+              hintText: "Type a message....",
+              //obscureText: false,
+            ),
+          ),
+        ),
+
+        //send button
+        Container(
+          margin: const EdgeInsets.only(right: 12.0),
+          child: IconButton(
+            onPressed: sendMessage,
+            icon: const Icon(Icons.arrow_upward),
+          ),
+        ),
+      ],
+    );
   }
 }
